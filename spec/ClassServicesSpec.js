c95957a34a3ba75bf238913ed6237e68562df888
utlight <joker_e@163.com>
2017-04-06 16:02:29 +0800

    refactor the ClassServices

--- spec/ClassServicesSpec.js
+++ spec/ClassServicesSpec.js
@@ -1,37 +1,37 @@
 describe('ClassManagement Spec', () => {
-    const ClassManagement = require('../lib/ClassManagement');
+    const ClassServices = require('../lib/ClassServices');
     const Student = require('../lib/Student');
     const Clazz = require('../lib/Clazz');
     it('should add student to clazz when given a student & class is exist', () => {
-        let classManagement = new ClassManagement();
+        let classServices = new ClassServices();
         let testStudent = new Student('name', '001', '汉族', '1', 120, 110, 120, 110);
         let existStudent = new Student('qspeng', '666', '汉族', '1', 120, 110, 120, 110);
-        classManagement.clazzs = [new Clazz(existStudent)];
+        classServices.clazzs = [new Clazz(existStudent)];
 
         let expectResult = [existStudent, testStudent];
 
-        classManagement.updateClazzs(testStudent);
-        expect(classManagement.clazzs[0].stuScore).toEqual(expectResult);
+        classServices.updateClazzs(testStudent);
+        expect(classServices.clazzs[0].stuScore).toEqual(expectResult);
     });
 
     it('should create clazz and add student when given a student & class is not exist', () => {
-        let classManagement = new ClassManagement();
+        let classServices = new ClassServices();
         let testStudent = new Student('name', '001', '汉族', '1', 120, 110, 120, 110);
         let expectResult = [testStudent];
 
-        classManagement.updateClazzs(testStudent);
-        expect(classManagement.clazzs[0].stuScore).toEqual(expectResult);
+        classServices.updateClazzs(testStudent);
+        expect(classServices.clazzs[0].stuScore).toEqual(expectResult);
     });
 
     it('should update clazzs when given a student is already in a clazz', () => {
-        let classManagement = new ClassManagement();
+        let classServices = new ClassServices();
         let updateStudent = new Student('name', '001', '汉族', '1', 120, 110, 120, 110);
         let existStudent = new Student('qspeng', '001', '汉族', '1', 120, 110, 120, 110);
-        classManagement.clazzs = [new Clazz(existStudent)];
+        classServices.clazzs = [new Clazz(existStudent)];
         let expectResult = [updateStudent];
 
-        classManagement.updateClazzs(updateStudent);
-        expect(classManagement.clazzs[0].stuScore).toEqual(expectResult);
+        classServices.updateClazzs(updateStudent);
+        expect(classServices.clazzs[0].stuScore).toEqual(expectResult);
     });
 
 });
